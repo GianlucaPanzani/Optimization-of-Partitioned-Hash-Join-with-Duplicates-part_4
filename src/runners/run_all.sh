21d179ac6c8d2b4383002105c0df2733844dcbaf
GianlucaPanzani <gianluca.panzani97@gmail.com>
2026-05-21 19:44:48 +0200

    hybrid exec

--- src/runners/run_all.sh
+++ src/runners/run_all.sh
@@ -4,7 +4,7 @@
 
 #./benchmark.sh hashjoin_omp grid/omp_grid_search.sh 5
 #./benchmark.sh hashjoin_mpi grid/mpi_grid_search.sh 5
-#./benchmark.sh hashjoin_hybrid grid/hybrid_grid_search.sh 5
+./benchmark.sh hashjoin_hybrid grid/hybrid_grid_search.sh 5
 
-./benchmark_for_scaling.sh hashjoin_mpi grid/strong_scaling_grid_search.sh 5
-./benchmark_for_scaling.sh hashjoin_mpi grid/weak_scaling_grid_search.sh 5
+#./benchmark_for_scaling.sh hashjoin_mpi grid/strong_scaling_grid_search.sh 5
+#./benchmark_for_scaling.sh hashjoin_mpi grid/weak_scaling_grid_search.sh 5
